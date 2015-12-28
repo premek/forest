70a2c1cff0ef67aba69c056af30130ec9ccf211f
premek <premysl.vyhnal@gmail.com>
2015-12-28 13:50:52 +0100

    update to love 0.10.0

--- src/conf.lua
+++ src/conf.lua
@@ -1,11 +1,11 @@
 function love.conf(t)
   t.window = t.window or t.screen
 
-  t.version = "0.9.1"                -- The LÖVE version this game was made for (string)
+  t.version = "0.10.0"                -- The LÖVE version this game was made for (string)
   t.window.title = "Forest"        -- The window title (string)
   t.title = "Forest"        -- The window title (string)
   t.window.fullscreen = false        -- Enable fullscreen (boolean)
-  t.window.fullscreentype = "normal" -- Standard fullscreen or desktop fullscreen mode (string)
+  --  t.window.fullscreentype = "normal" -- Standard fullscreen or desktop fullscreen mode (string)
 
   t.window.width = 832 -- t.screen.width in 0.8.0 and earlier
   t.window.height = 832 -- t.screen.height in 0.8.0 and earlier
